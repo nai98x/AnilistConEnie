fff85a25e924d11467d5a4f5582cedcfa62c345a
Mariano Burguete <corsaiir@gmail.com>
2021-06-29 20:43:16 -0300

    rework comando bestgirl

--- AnilistESP/Modulos/Interactuar.cs
+++ AnilistESP/Modulos/Interactuar.cs
@@ -27,40 +27,70 @@ namespace AnilistESP
         [Command("bestgirl"), Description("Best girl de Konosuba."), Cooldown(1, 300, CooldownBucketType.Guild)]
         public async Task BestGirl(CommandContext ctx)
         {
-            Random rnd = new Random();
-            int random = rnd.Next(3);
-            switch (random)
+            if(ctx.Guild.Id == 701813281718927441)
             {
-                case 0:
-                    await ctx.Channel.SendMessageAsync(embed: new DiscordEmbedBuilder
-                    {
-                        Footer = funciones.GetFooter(ctx),
-                        Color = DiscordColor.Cyan,
-                        Title = "Best Girl (Konosuba)",
-                        Description = "Aqua",
-                        ImageUrl = "https://media.discordapp.net/attachments/816379048477065217/848254027639816202/b89362-ibkc0eoECaW1.png"
-                    }).ConfigureAwait(false);
-                    break;
-                case 1:
-                    await ctx.Channel.SendMessageAsync(embed: new DiscordEmbedBuilder
-                    {
-                        Footer = funciones.GetFooter(ctx),
-                        Color = DiscordColor.Yellow,
-                        Title = "Best Girl (Konosuba)",
-                        Description = "Darkness",
-                        ImageUrl = "https://media.discordapp.net/attachments/816379048477065217/848254149966561300/b89363-mm21Ll4NegUD.png"
-                    }).ConfigureAwait(false);
-                    break;
-                case 2:
-                    await ctx.Channel.SendMessageAsync(embed: new DiscordEmbedBuilder
-                    {
-                        Footer = funciones.GetFooter(ctx),
-                        Color = DiscordColor.Red,
-                        Title = "Best Girl (Konosuba)",
-                        Description = "Megumin",
-                        ImageUrl = "https://media.discordapp.net/attachments/816379048477065217/848254129084432428/b89361-x71P6YLrndd8.png"
-                    }).ConfigureAwait(false);
-                    break;
+                DiscordRole rol;
+                Random rnd = new Random();
+                int random = rnd.Next(4);
+                switch (random)
+                {
+                    case 0:
+                        rol = ctx.Guild.GetRole(725562290148999240);
+                        await ctx.Channel.SendMessageAsync(embed: new DiscordEmbedBuilder
+                        {
+                            Footer = funciones.GetFooter(ctx),
+                            Color = DiscordColor.Cyan,
+                            Title = "Aqua",
+                            Description = $"Triunfo de los {rol.Mention}!",
+                            ImageUrl = "https://media.discordapp.net/attachments/816379048477065217/848254027639816202/b89362-ibkc0eoECaW1.png"
+                        }).ConfigureAwait(false);
+                        break;
+                    case 1:
+                        rol = ctx.Guild.GetRole(725425507797303409);
+                        await ctx.Channel.SendMessageAsync(embed: new DiscordEmbedBuilder
+                        {
+                            Footer = funciones.GetFooter(ctx),
+                            Color = DiscordColor.Yellow,
+                            Title = "Darkness",
+                            Description = $"Triunfo de los {rol.Mention}!",
+                            ImageUrl = "https://media.discordapp.net/attachments/816379048477065217/848254149966561300/b89363-mm21Ll4NegUD.png"
+                        }).ConfigureAwait(false);
+                        break;
+                    case 2:
+                        rol = ctx.Guild.GetRole(718187374953103389);
+                        await ctx.Channel.SendMessageAsync(embed: new DiscordEmbedBuilder
+                        {
+                            Footer = funciones.GetFooter(ctx),
+                            Color = DiscordColor.Red,
+                            Title = "Megumin",
+                            Description = $"Triunfo de los {rol.Mention}!",
+                            ImageUrl = "https://media.discordapp.net/attachments/816379048477065217/848254129084432428/b89361-x71P6YLrndd8.png"
+                        }).ConfigureAwait(false);
+                        break;
+                    case 3:
+                        rol = ctx.Guild.GetRole(859207787703304222);
+                        await ctx.Channel.SendMessageAsync(embed: new DiscordEmbedBuilder
+                        {
+                            Footer = funciones.GetFooter(ctx),
+                            Color = DiscordColor.Green,
+                            Title = "Kazuma",
+                            Description = $"Triunfo de los {rol.Mention}!",
+                            ImageUrl = "https://media.discordapp.net/attachments/854384291734356009/859576198748176453/b89364-7Th8Tv1XKJtt.png"
+                        }).ConfigureAwait(false);
+                        break;
+                }
+            }
+            else
+            {
+                var msg = await ctx.Channel.SendMessageAsync(embed: new DiscordEmbedBuilder
+                {
+                    Title = "Comando no habilitado",
+                    Description = $"Este comando no puede ser ejecutado en este servidor",
+                    Footer = funciones.GetFooter(ctx),
+                    Color = funciones.GetColor()
+                });
+                await Task.Delay(5000);
+                await funciones.BorrarMensaje(ctx, msg.Id);
             }
         }
 

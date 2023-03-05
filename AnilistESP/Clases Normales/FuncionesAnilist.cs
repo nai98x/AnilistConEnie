ddd17b95f2ddcf4f000fbac8399cd5a40ce45814
Nai <marianoburguete@gmail.com>
2023-03-04 21:05:18 -0300

    Update FuncionesAnilist.cs

--- AnilistESP/Clases Normales/FuncionesAnilist.cs
+++ AnilistESP/Clases Normales/FuncionesAnilist.cs
@@ -345,11 +345,11 @@ namespace AnilistESP
 
                                 if (status == "COMPLETED")
                                 {
-                                    scoresList.Add($"{Formatter.MaskedUrl(name, new Uri(url))} - {scoreF} (<@{discordUser.UserId}>)\n");
+                                    scoresList.Add($"<@{discordUser.UserId}> - {Formatter.MaskedUrl(scoreF, new Uri(url))}\n");
                                 }
                                 else
                                 {
-                                    scoresList.Add($"{Formatter.MaskedUrl(name, new Uri(url))} - {scoreF} {Formatter.InlineCode($"{Funciones.UppercaseFirst(status)} - Progress: {pro}")} (<@{discordUser.UserId}>)\n");
+                                    scoresList.Add($"<@{discordUser.UserId}> - {Formatter.MaskedUrl(scoreF, new Uri(url))} {Formatter.InlineCode($"{Funciones.UppercaseFirst(status)} - Progress: {pro}")}\n");
                                 }
 
                                 registros++;
